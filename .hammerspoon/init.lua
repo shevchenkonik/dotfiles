98eaa617e9890e6dd7427cfd298abfe614955a5c
Nik Shevchenko <shevchenko.nik@icloud.com>
2020-04-30 23:48:30 +0300

    Music & auto reloading

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,9 +2,21 @@ hyper = { "cmd", "ctrl", "shift", "alt"}
 
 require "apps"
 require "window-management"
+require "music"
 
-hs.hotkey.bind(hyper, "0", function()
-    hs.reload()
-end)
+-- Watcher
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+
+configWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
 hs.notify.new({ title="Hammerspoon", informativeText="Config loaded" }):send()
