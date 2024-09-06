bf644c27a63333a11f3acba93b478f9245b6142a
Nik <shevchenko.nik@icloud.com>
2024-09-06 17:03:36 +0200

    Updated structure of the files. Start of refactoring to new representation format

---
+++ home/hammerspoon/init.lua
@@ -0,0 +1,16 @@
+hyper = { "cmd", "ctrl", "shift", "alt"}
+apps = {
+    WEBSTORM = "Webstorm",
+    CHROME = "Google Chrome",
+    ITERM = "iTerm",
+    TELEGRAM = "Telegram",
+    MAIL = "Mail",
+    SPOTIFY = "Spotify",
+    THINGS = "Things",
+    OBSIDIAN = "Obsidian",
+    ONEPASSWORD = "1Password"
+}
+
+require "watcher"
+require "apps"
+require "window-management"
