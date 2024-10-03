30e3c477f88a37d38cc7df71005a14fb60ab282f
Nik <shevchenko.nik@icloud.com>
2024-10-03 18:17:31 +0200

    Added alias for mkdir & cd

--- home/.config/shell/shortcuts.sh
+++ home/.config/shell/shortcuts.sh
@@ -8,3 +8,29 @@ alias lsa="ls -lhFaG"
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+
+# Creates a new directory and enters it
+function mkd() {
+    mkdir -p $@ && cd $_
+}
+
+# Print all aliases
+function printAliases() {
+    local result=""
+
+    if [[ $# -eq 0 ]]; then
+        result=$(alias)
+    else
+        result=$(alias | rg "^$@")
+    fi
+
+    if [[ -z "$result" ]]; then
+        which "$@"
+    else
+        echo "Aliases:"
+        echo "$result"
+        echo
+        echo "Which:"
+        which "$@"
+    fi
+}
