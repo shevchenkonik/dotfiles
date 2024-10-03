907a6a0300a9f0502e582591d01012c0571350ae
Nik <shevchenko.nik@icloud.com>
2024-10-03 18:05:23 +0200

    Added function to initialization of git repo

--- home/.config/shell/apps/git.sh
+++ home/.config/shell/apps/git.sh
@@ -5,3 +5,8 @@ alias ga="git add "
 alias gb="git branch "
 alias gp="git push "
 alias gc="git commit -m "
+
+# Initialize git repository and make an initial commit
+function ginit() {
+    git init && git commit -m "Initial commit" --allow-empty
+}
