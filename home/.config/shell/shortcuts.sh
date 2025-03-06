8d5628a22aede248675f171ef7b3ffd654aaaddb
Nik <shevchenko.nik@icloud.com>
2025-03-06 10:58:09 +0100

    updated shortcuts

--- home/.config/shell/shortcuts.sh
+++ home/.config/shell/shortcuts.sh
@@ -1,23 +1,22 @@
-# Common Aliases
 alias c="clear"
 alias q="exit"
+
 alias rmrf="rm -rf"
+
+alias p="cd ~/Projects"
+
 alias ls="eza --icons=always"
 alias lsa="ls -lhFaG"
-alias dev="cd ~/Projects"
-
 alias lls='eza -lh --group-directories-first --icons --hyperlink'
 alias llsa='lls -a'
 alias llt='eza --tree --level=2 --long --icons --git'
 alias llta='llt -a'
+alias l.="eza -a | grep -E '^\.'"
 
 # Navigation Aliases
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
-
-# Common Aliases
-alias day="/usr/libexec/corebrightnessdiag nightshift-internal | awk '/sunrise|sunset/ {print $4}'"
 
 # Creates a new directory and enters it
 function mkd() {
